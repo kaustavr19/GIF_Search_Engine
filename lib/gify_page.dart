a63cb573c89bc19a7e816c76a993d08f9839a3d3
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-06 12:10:19 +0530

    appbar main body textfield & color added

--- lib/gify_page.dart
+++ lib/gify_page.dart
@@ -10,6 +10,20 @@ class GifyPage extends StatefulWidget {
 class _GifyPageState extends State<GifyPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Vx.green800,
+      appBar: AppBar(title: Text('GIFY App'), backgroundColor: Vx.green500),
+      body: Theme(
+        data: ThemeData.dark(),
+        child: Column(
+          children: [
+            "The Best GIF Engine".text.white.xl3.make().objectCenter(),
+            TextField(
+              decoration: InputDecoration(),
+            ),
+          ],
+        ).p16(),
+      ),
+    );
   }
 }
