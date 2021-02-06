0ebb84693e2633439f24d42c69dbd3458d6c1ef8
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-06 11:08:26 +0530

    gify spearate page done and linked

---
+++ lib/gify_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:velocity_x/velocity_x.dart';
+
+class GifyPage extends StatefulWidget {
+  @override
+  _GifyPageState createState() => _GifyPageState();
+}
+
+class _GifyPageState extends State<GifyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
