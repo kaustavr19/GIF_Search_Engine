f1ff671e971e1f43d5c0ba6f432926e697ac784c
kaustavr19 <kaustavr19@users.noreply.github.com>
2021-02-06 15:37:58 +0530

    main app ready

--- lib/gify_page.dart
+++ lib/gify_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:velocity_x/velocity_x.dart';
@@ -9,8 +11,18 @@ class GifyPage extends StatefulWidget {
 
 class _GifyPageState extends State<GifyPage> {
   final TextEditingController controller = TextEditingController();
+  final String url =
+      "https://api.giphy.com/v1/gifs/search?api_key=pe3ZWyViWslU9f87eJGnEdq3vg4gYkJ0&limit=25&offset=0&rating=G&lang=en&q=";
 
   var data;
+
+  getData(String searchInput) async {
+    var res = await http.get(url + searchInput);
+    data = jsonDecode(res.body)["data"];
+    setState(() {});
+    print(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +46,9 @@ class _GifyPageState extends State<GifyPage> {
                 ),
                 30.widthBox,
                 RaisedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    getData(controller.text);
+                  },
                   shape: Vx.roundedSm,
                   child: Text("Go!"),
                 ).h8(context),
@@ -42,9 +56,19 @@ class _GifyPageState extends State<GifyPage> {
             ).p8(),
             VxConditional(
               condition: data != null,
-              builder: (context) => Text('Data available'),
-              fallback: (context) => "Nothing found".text.make(),
-            ),
+              builder: (context) => GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: context.isMobileTypeHandset ? 2 : 3,
+                ),
+                itemBuilder: (context, index) {
+                  final url =
+                      data[index]["images"]["fixed_height"]["url"].toString();
+                  return Image.network(url).card.roundedSM.make();
+                },
+                itemCount: data.length,
+              ),
+              fallback: (context) => "Nothing found :(".text.gray500.make(),
+            ).h(context.percentHeight * 70),
           ],
         ).p16(),
       ),
